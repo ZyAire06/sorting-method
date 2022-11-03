a276e9cb6de8426ca90a5a423ca97c161cc3edf1
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-11-03 14:10:16 -0400

    reoder the sorting list

--- products_sorting.js
+++ products_sorting.js
@@ -1,16 +1,17 @@
 
 let products = [ { prodId: 2, item: "Notepads (12 pack)", price: 12.29},
                  { prodId: 12, item: "Black Pens  (12 pack)", price: 5.70},
-                 { prodId: 12, item: "Stapler", price: 12.79}
+                 { prodId: 22, item: "Stapler", price: 12.79}
 ];
 
 products.sort(
     function getProductsAscending(a, b){
-        if(a < b) return -1;
-        else if(a==b) return 0;
-        else return 1;
+        if(a.item < b.item) return 1;
+        else if(a.item==b.item) return 0;
+        else return -1;
     }
 )
+console.log(products);
 
 let numOfProducts = products.length;
 
